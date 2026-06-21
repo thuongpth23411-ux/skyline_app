a0b377447b547e6acf84d2799670b8c3289cb8a6
Nguyễn Ngọc Tường Vy <vynnt23411@st.uel.edu.vn>
2026-06-21 17:58:51 +0700

    home-page

--- app/src/main/java/com/skyline/model/HomeModels.kt
+++ app/src/main/java/com/skyline/model/HomeModels.kt
@@ -5,18 +5,18 @@ import androidx.annotation.DrawableRes
 data class Promotion(
     val title: String,
     val date: String,
-    @DrawableRes val imageRes: Int
+    @param:DrawableRes val imageRes: Int
 )
 
 data class Destination(
     val country: String,
     val title: String,
-    @DrawableRes val imageRes: Int
+    @param:DrawableRes val imageRes: Int
 )
 
 data class Experience(
     val tag: String,
     val title: String,
     val description: String,
-    @DrawableRes val imageRes: Int
+    @param:DrawableRes val imageRes: Int
 )
