1dfd77308fa69df2950631d82a94bcd95b27801a
thienthao <tranthienthao81@gmail.com>
2026-07-12 13:41:27 +0700

    000

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use("/images", express.static("images"));
 
 // Routes
